0e70f75c6d02693378da4499d36fa9548919fb6a
Brody Osterbuhr <34581663+BOsterbuhr@users.noreply.github.com>
2024-08-05 21:02:08 -0500

    update

--- examples/brain-mri/experiment/startup-hook.sh
+++ examples/brain-mri/experiment/startup-hook.sh
@@ -1,3 +1,3 @@
-pip install scikit-image==0.19.2 pachyderm-sdk==2.7.2
+pip install scikit-image==0.19.2 pachyderm-sdk[cdr]==2.11.0rc2 pachyderm-sdk==2.11.0rc2
 pip install scikit-learn
 pip install opencv-python-headless
